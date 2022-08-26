8ac9921cabc72eeb9827df9bb54036b6772f5ae0
Aleksey Ushakov <statik2002@mail.ru>
2022-08-26 22:48:36 +0800

    ifmain

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 load_dotenv()
-print(os.environ['TOKEN'])
+TOKEN = os.environ['TOKEN']
 
 
-def shorten_link(token, url):
+def shorten_link(token, long_url):
     headers = {
         'Authorization': f'Bearer {token}',
     }
@@ -18,29 +18,10 @@ def shorten_link(token, url):
     request_url = "https://api-ssl.bitly.com/v4/shorten"
 
     create_bitlink = {
-        "long_url": url,
+        "long_url": long_url,
     }
 
     response = requests.post(request_url, headers=headers, json=create_bitlink)
-    response.raise_for_status()
-
-    return json.loads(response.text)['id']
-
-
-def bitlink_information_from_url(token, url):
-    headers = {
-        'Authorization': f'Bearer {token}',
-    }
-
-    parsed_url = urlparse(url)
-
-    request_url = "https://api-ssl.bitly.com/v4/shorten"
-
-    body = {
-        "long_url": url,
-    }
-
-    response = requests.post(request_url, headers=headers, json=body)
     response.raise_for_status()
 
     return json.loads(response.text)['id']
@@ -63,26 +44,25 @@ def count_clicks(token, link):
     return json.loads(response.text)['total_clicks']
 
 
-def is_bitlink(url):
-    url_parsed = urlparse(url)
-    print(url, url_parsed)
+def is_bitlink(input_url):
+    url_parsed = urlparse(input_url)
     if url_parsed.netloc == 'bit.ly':
         return True
-
     return False
 
 
-url = input()
+if __name__ == '__main__':
+    url = input()
 
-if is_bitlink(url):
-    try:
-        total_clicks = count_clicks(TOKEN, url)
-        print(total_clicks)
-    except requests.exceptions.HTTPError as error:
-        exit(f"Не могу получить кол-во кликов по причине: {error}")
-else:
-    try:
-        shorten_link = shorten_link(TOKEN, url)
-        print(shorten_link)
-    except requests.exceptions.HTTPError as error:
-        exit(f"Не могу получить информацию по ссылке по причине: {error}")
+    if is_bitlink(url):
+        try:
+            total_clicks = count_clicks(TOKEN, url)
+            print(total_clicks)
+        except requests.exceptions.HTTPError as error:
+            exit(f"Не могу получить кол-во кликов по причине: {error}")
+    else:
+        try:
+            shorten_link = shorten_link(TOKEN, url)
+            print(shorten_link)
+        except requests.exceptions.HTTPError as error:
+            exit(f"Не могу получить информацию по ссылке по причине: {error}")
